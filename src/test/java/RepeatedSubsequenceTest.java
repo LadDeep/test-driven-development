21550ec447015691152a918653c6003e90cb49c8
Mayank Patel <mayank@mayanks-air.home>
2023-02-19 21:30:33 -0400

    add test for wrong repeated subsequence string

--- src/test/java/RepeatedSubsequenceTest.java
+++ src/test/java/RepeatedSubsequenceTest.java
@@ -1,6 +1,7 @@
 import org.example.RepeatedSubsequence;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class RepeatedSubsequenceTest {
@@ -9,6 +10,12 @@ public class RepeatedSubsequenceTest {
     public void returnsTrue() {
         RepeatedSubsequence repeatedSubsequence = new RepeatedSubsequence();
         assertTrue(repeatedSubsequence.isRepeatedSubsequence("abcab"));
+    }
+
+    @Test
+    public void returnsFalse() {
+        RepeatedSubsequence repeatedSubsequence = new RepeatedSubsequence();
+        assertFalse(repeatedSubsequence.isRepeatedSubsequence("aab"));
     }
 
 }
