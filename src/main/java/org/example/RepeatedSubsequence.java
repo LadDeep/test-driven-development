bc5c3e33b0a11c598b1b6ec58e9ae354fbbb70b1
Deep Lad <deeplad2000@gmail.com>
2023-02-22 18:43:06 -0400

    Add logic for repeated subsequence

--- src/main/java/org/example/RepeatedSubsequence.java
+++ src/main/java/org/example/RepeatedSubsequence.java
@@ -5,7 +5,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RepeatedSubsequence {
+
     public boolean isRepeatedSubsequence(String str){
-        return true;
+        final int DECIMAL = 10;
+
+        Map<String, ArrayList<String>> subsequenceMap = new HashMap<>();
+        for (int i = 0; i < str.length()-1; i++) {
+            for (int j = i+1; j < str.length(); j++) {
+                String currentSubsequence = str.charAt(i)+""+str.charAt(j);
+                String indexPair = String.valueOf(i).concat(String.valueOf(j));
+                if(subsequenceMap.containsKey(currentSubsequence)){
+                    int arrayLength = subsequenceMap.get(currentSubsequence).size();
+                    for (int k = 0; k < arrayLength; k++) {
+                        if(subsequenceMap.get(currentSubsequence).get(k).charAt(0)!=Character.forDigit(i,DECIMAL) && subsequenceMap.get(currentSubsequence).get(k).charAt(1)!=Character.forDigit(j, DECIMAL) ) {
+                            subsequenceMap.get(currentSubsequence).add(indexPair);
+                            return true;
+                        }
+                    }
+                } else {
+                    ArrayList<String> newIndexPairList = new ArrayList<>();
+                    newIndexPairList.add(indexPair);
+                    subsequenceMap.put(currentSubsequence, newIndexPairList);
+                }
+            }
+        }
+        return false;
     }
 }
