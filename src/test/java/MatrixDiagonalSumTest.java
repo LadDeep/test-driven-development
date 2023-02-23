e73a8cbb8305ff1e31f41bdb6d99dcbd35909dd9
Deep Lad <deeplad2000@gmail.com>
2023-02-22 21:02:11 -0400

    Add test case to check matrix is empty

---
+++ src/test/java/MatrixDiagonalSumTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MatrixDiagonalSumTest {
+    @Test
+    public void testWhenMatrixIsEmpty(){
+        MatrixDiagonalSum matrixDiagonalSum = new MatrixDiagonalSum();
+        double [][]matrix={{}};
+        assertTrue(matrixDiagonalSum.isEmpty(matrix));
+    }
+}
