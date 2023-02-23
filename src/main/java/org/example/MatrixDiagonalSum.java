68d067d27c854ea87fc2d34bf9e3727553965e71
Mayank Patel <mayank@mayanks-air.home>
2023-02-22 21:49:28 -0400

    add function to check square and odd matrix

--- src/main/java/org/example/MatrixDiagonalSum.java
+++ src/main/java/org/example/MatrixDiagonalSum.java
@@ -10,8 +10,11 @@ public class MatrixDiagonalSum {
     }
 
     public boolean isSquare(double[][] matrix) {
+        if(matrix.length %2 == 0) {
+            return false;
+        }
         for (int i = 0; i < matrix.length; i++) {
-            if(matrix[i].length != matrix.length) {
+            if((matrix[i].length != matrix.length) || (matrix[i].length % 2 == 0)) {
                 return false;
             }
         }
