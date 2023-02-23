9c4aed7f1ed7642ccadf1c2b0823a8982cb6cb6a
Mayank Patel <mayank@mayanks-air.home>
2023-02-22 21:10:22 -0400

    add funtion to check empty matrix

---
+++ src/main/java/org/example/MatrixDiagonalSum.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class MatrixDiagonalSum {
+
+    public boolean isEmpty(double[][] matrix) {
+        if(matrix[0].length == 0) {
+            return true;
+        }
+        return false;
+    }
+}
--- src/test/java/MatrixDiagonalSumTest.java
+++ src/test/java/MatrixDiagonalSumTest.java
@@ -1,3 +1,4 @@
+import org.example.MatrixDiagonalSum;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
